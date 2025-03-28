7b02051731e9c8cc5209bde7d8932c9344b942f8
zvezd <adelevnikita@gmail.com>
2025-03-28 23:30:56 +0200

    naebal

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,35 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./transition.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import { Suspense, lazy } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const MainPage = lazy(() => import("../pages/MainPage"));
 const ComicsPage = lazy(() => import("../pages/ComicsPage"));
 const Page404 = lazy(() => import("../pages/Error404"));
 const AppHeader = lazy(() => import("../appHeader/AppHeader"));
 const SingleComicsPage = lazy(() => import("../pages/SingleComicsPage"));
+
+const AnimatedRoutes = () => {
+  const location = useLocation();
+
+  return (
+    <TransitionGroup>
+      <CSSTransition key={location.pathname} classNames="fade" timeout={300}>
+        <Routes location={location}>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/comics" element={<ComicsPage />} />
+          <Route path="/comics/:comicId" element={<SingleComicsPage />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+};
 
 const App = () => {
   return (
@@ -14,12 +38,7 @@ const App = () => {
         <Suspense fallback={<div>Loading...</div>}>
           <AppHeader />
           <main>
-            <Routes>
-              <Route path="/" element={<MainPage />} />
-              <Route path="/comics" element={<ComicsPage />} />
-              <Route path="/comics/:comicId" element={<SingleComicsPage />} />
-              <Route path="*" element={<Page404 />} />
-            </Routes>
+            <AnimatedRoutes />
           </main>
         </Suspense>
       </div>
@@ -28,3 +47,9 @@ const App = () => {
 };
 
 export default App;
+
+// key={location.pathname} указывает React, что каждый маршрут должен рассматриваться как уникальный элемент. Это важно, потому что:
+
+//     CSSTransition работает на основе ключей (key), и при каждом изменении key компонент перерисовывается.
+//     Если key остается неизменным, React не перерисовывает компонент, а просто обновляет его.
+//     Если key изменяется (например, с / на /comics), CSSTransition понимает, что это новый элемент, и запускает анимацию входа/выхода.
